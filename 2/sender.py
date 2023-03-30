bafdea46b8c66b1da1418733bde45957d20a54d6
Bardia Khosravi <bardiakhosraviesmaeili@gmail.com>
2023-03-30 16:19:55 +0430

    second practice

---
+++ 2/sender.py
@@ -0,0 +1,15 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+ch = connection.channel()
+
+ch.queue_declare(queue='first', durable=True)
+
+message = 'This is the message from the sender.'
+
+ch.basic_publish(exchange='', routing_key='first', body=message, properties=pika.BasicProperties(delivery_mode=2, ))
+
+print('send message')
+
+connection.close()
